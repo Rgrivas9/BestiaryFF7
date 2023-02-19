11c3040cb7a4f87262e7450b0aa449c70a846227
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-02-19 23:54:39 +0100

    range doble

--- src/components/MinMaxFilter.jsx
+++ src/components/MinMaxFilter.jsx
@@ -1,6 +1,47 @@
+import { useRef, useState } from "react";
+import "./MinMaxFilter.css";
 const MinMaxFilter = () => {
-    return(
-        <div></div>
-    )
-}
-export default MinMaxFilter
+  const rangeMin = useRef(null);
+  const rangeMax = useRef(null);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(100);
+  const handleChange = () => {
+    let min = rangeMin.current.value;
+    let max = rangeMax.current.value;
+    setMin(min);
+    setMax(max);
+  };
+  return (
+    <div>
+      <h3>{min}</h3>
+      <div className="minMaxInput">
+      <input
+          className="maxInput"
+          type="range"
+          ref={rangeMax}
+          min={min}
+          max={100}
+          defaultValue={100}
+          style={{ width: `${100 - min}%`}}
+          onChange={() => {
+            handleChange();
+          }}
+        />
+        <input
+          className="minInput"
+          type="range"
+          ref={rangeMin}
+          min={0}
+          max={max - 1}
+          defaultValue={0}
+          style={{ width: `${max-1}%` }}
+          onChange={() => {
+            handleChange();
+          }}
+        />
+      </div>
+      <h3>{max}</h3>
+    </div>
+  );
+};
+export default MinMaxFilter;
--- src/pages/Bestiary2d.jsx
+++ src/pages/Bestiary2d.jsx
@@ -1,17 +1,19 @@
 import { useEffect } from "react";
 import Header from "../components/header";
+import MinMaxFilter from "../components/MinMaxFilter";
 
 const Bestiary2d = () => {
-    useEffect(()=>{
+    /* useEffect(()=>{
         fetch('https://63f26f29f28929a9df5a9085.mockapi.io/enemies')
         .then((res) => res.json())
         .then((res) => {
         console.log(res)
       });
-    },[])
+    },[]) */
   return (
     <>
       <Header></Header>
+      <MinMaxFilter/>
       <main>
         <h1>Bestiary2d</h1>
       </main>
