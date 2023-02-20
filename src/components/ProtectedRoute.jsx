cc41594a472c4d8248ffc181f2c5b8ff3dbb7517
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-02-20 08:54:16 +0100

    ..

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({children}) => {
     const {_3d} = useContext(_3dContext)
-    if (!_3d) {
+    if (_3d==null) {
         return <Navigate to='/'/>
     }
     return children
